afe66c21999fcc31d5567a39aedd95032773dfda
lzdnico <596049368@qq.com>
2021-12-07 20:21:20 +0800

    提升代码健壮性

--- Python/changefilename.py
+++ Python/changefilename.py
@@ -1,21 +1,28 @@
 import os
-#path=input('请输入文件路径(结尾加上/)：')       
-path = "D:/GitHub/Python/test/"
-#获取该目录下所有文件，存入列表中
-fileList=os.listdir(path)
 
-n=0
-for i in fileList:
-    
-    #设置旧文件名（就是路径+文件名）
-    oldname=path+ os.sep + fileList[n]   # os.sep添加系统分隔符
 
-    needname = fileList[n].split('.txt')[0]
+def Change_File_Name(mypath,split_str,suffix_str):
     
-    #设置新文件名
-    newname=path + os.sep + needname +str(n+1)+'.txt'
-    
-    os.rename(oldname,newname)   #用os模块中的rename方法对文件改名
-    print(oldname,'======>',newname)
-    
-    n+=1
+    fileList=os.listdir(mypath)
+
+    for i in fileList:
+        try:
+            #设置旧文件名（就是路径+文件名）
+            oldname = mypath + os.sep + fileList[i]   # os.sep添加系统分隔符
+
+            lens  = len(fileList[i].split(split_str))
+
+            if lens == 2:
+
+                needname = fileList[i].split(split_str)[0]
+                
+                newname=mypath + os.sep + needname + suffix_str
+                
+                os.rename(oldname,newname)   #用os模块中的rename方法对文件改名
+
+                print(oldname,'======>',newname)
+                      
+        except:
+            pass 
+
+Change_File_Name("D:/GitHub/Python/test/",".doc","测试.doc")
